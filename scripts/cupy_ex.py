6638a5f550597f77f72f13d2ff5cde0d5dde6860
Lucas Ford <lcford2@ncsu.edu>
2022-09-23 16:07:43 -0400

    cupy example

--- scripts/cupy_ex.py
+++ scripts/cupy_ex.py
@@ -1,7 +1,107 @@
 import cupy as cp
+import numpy as np
+from time import perf_counter as timer
 
-arr = cp.random.rand(1000) * 10
+# Ab = c (solve for b)
 
-print(arr.mean())
+def array_creation(size):
+    # array creation
+    s = timer()
+    x_cpu = np.random.random((size, size, size))
+    e = timer()
+    print(f"Array Creation GPUU Time: {e-s:.4} seconds")
+
+    s = timer()
+    x_gpu = cp.random.random((size, size, size))
+    # sync GPU and GPUU to make sure data gen is done
+    cp.cuda.Stream.null.synchronize()
+    e = timer()
+    print(f"Array Creation GPU Time: {e-s:.4} seconds")
+    return x_cpu, x_gpu
+
+
+def array_scalar_multiply(x_cpu, x_gpu, scalar):
+    s = timer()
+    x_cpu = x_cpu * scalar
+    e = timer()
+    print(f"Array Multiplication GPUU Time: {e-s:.4} seconds")
+
+    s = timer()
+    x_gpu = x_gpu * scalar
+    # sync GPU and GPUU to make sure data gen is done
+    cp.cuda.Stream.null.synchronize()
+    e = timer()
+    print(f"Array Multiplication GPU Time: {e-s:.4} seconds")
+
+
+def multiple_operations(x_cpu, x_gpu):
+    s = timer()
+    x_cpu = x_cpu * 1.5
+    x_cpu = x_cpu + x_cpu
+    x_cpu = x_cpu * x_cpu
+    mean = x_cpu.mean()
+    e = timer()
+    print(f"Multiple Ops GPUU Time: {e-s:.4} seconds; Answer={mean:.2f}")
+
+    s = timer()
+    x_gpu = x_gpu * 1.5
+    x_gpu = x_gpu + x_gpu
+    x_gpu = x_gpu * x_gpu
+    mean = x_gpu.mean()
+    # sync GPU and GPUU to make sure data gen is done
+    cp.cuda.Stream.null.synchronize()
+    e = timer()
+    print(f"Multiple Ops GPU Time: {e-s:.4} seconds; Answer={mean:.2f}")
+
+
+def solve_linear_system(size):
+    X = np.random.random((size, size))
+    y = np.random.random((size))
+    s = timer()
+    x = np.linalg.solve(X, y)
+    e = timer()
+    print(f"CPU Linear System Solve: {e - s:.4f} seconds; Answer Mean: {x.mean():.4f}")
+
+    X = cp.random.random((size, size))
+    y = cp.random.random((size))
+    cp.cuda.Stream.null.synchronize()
+    s = timer()
+    x = cp.linalg.solve(X, y)
+    cp.cuda.Stream.null.synchronize()
+    e = timer()
+    print(f"GPU Linear System Solve: {e - s:.4f} seconds; Answer Mean: {x.mean():.4f}")
+
+
+def matrix_multiplication(size):
+    X = np.random.random((size, size))
+    s = timer()
+    result = np.matmul(X, X)
+    e = timer()
+    print(f"CPU Mat Mul: {e - s:.4f} seconds; Answer Mean: {result.mean():.4f}")
+
+    # X = cp.random.random((size, size))
+    X = cp.asarray(X)
+    cp.cuda.Stream.null.synchronize()
+    s = timer()
+    result = cp.empty_like(X)
+    cp.matmul(X, X, out=result)
+    cp.cuda.Stream.null.synchronize()
+    e = timer()
+    print(f"GPU Mat Mul: {e - s:.4f} seconds; Answer Mean: {result.mean():.4f}")
+
+
+if __name__ == "__main__":
+    # x_cpu, x_gpu = array_creation(1000)
+    # array_scalar_multiply(x_cpu, x_gpu, 10)
+    # multiple_operations(x_cpu, x_gpu)
+    # solve_linear_system(10000)
+    matrix_multiplication(5000)
+
+
+
+
+
+
+
 
 
